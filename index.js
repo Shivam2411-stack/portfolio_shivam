1b1eb307d0304f5f1013865dcd75905942a5fb40
Shivam Pal <nandalalpal240@gmail.com>
2025-07-27 20:05:52 +0530

    portfolio_files_1st_commit
    Lets go jai shri ram

---
+++ index.js
@@ -0,0 +1,12 @@
+
+// sidemenu openmenu()
+
+function openmenu() {
+    var sidemenu = document.getElementById("sidemenu");
+    sidemenu.style.right = "0";
+}
+// sidemenu closemenu()
+function closemenu() {
+    var sidemenu = document.getElementById("sidemenu");
+    sidemenu.style.right = "-200px";
+}
